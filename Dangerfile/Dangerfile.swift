adc56f2932a0874563d3d77eb24677f71e3d6c8b
佐藤汰一 <satoutaichi@satoutaichinoMacBook-Air.local>
2025-02-18 22:04:56 +0900

    ファイル差分の確認

--- Dangerfile/Dangerfile.swift
+++ Dangerfile/Dangerfile.swift
@@ -35,13 +35,13 @@ let changeFiles = (danger.git.createdFiles + danger.git.modifiedFiles).filter { 
 for target in targets {
     
     let targetFiles = changeFiles.filter { $0.hasPrefix(target.directory) }
-    let violations = SwiftLint.lint(.files(changeFiles),
+    let violations = SwiftLint.lint(.files(targetFiles),
                                     inline: true,
                                     configFile: target.configPath,
                                     quiet: false,
                                     swiftlintPath: lintPath)
-    message("SwiftLintでの対象ファイル数は\(changeFiles.count), 指摘数は\(violations.count)件です。")
-    changeFiles.forEach {
+    message("SwiftLintでの対象ファイル数は\(targetFiles.count), 指摘数は\(violations.count)件です。")
+    targetFiles.forEach {
         message("SwiftLint対象ファイル: " + $0.name)
     }
 }
