ecbf95555ef8b98f996b92b78d8110c93e7e9358
佐藤汰一 <satoutaichi@satoutaichinoMacBook-Air.local>
2025-02-18 21:04:26 +0900

    誤ったconfigパスを設定

--- Dangerfile/Dangerfile.swift
+++ Dangerfile/Dangerfile.swift
@@ -28,7 +28,7 @@ struct LintTarget {
 let swiftLintCmdPath = ".build/artifacts/swiftlintplugins/SwiftLintBinary/SwiftLintBinary.artifactbundle/swiftlint-0.58.2-macos/bin/swiftlint"
 let lintPath = SwiftLint.SwiftlintPath.bin(swiftLintCmdPath)
 let targets: [LintTarget] = [
-    LintTarget(directory: "DangerSample", configPath: "DangerSample/.swiftlint.yml")
+    LintTarget(directory: "DangerSample", configPath: "sfefseDangerSample/.swiftlint.yml")
 ]
 
 for target in targets {
